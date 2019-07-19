93c301e05b3a417442caf9215b0ae74402bfaeff
yadong.zhang <yadong.zhang0415@gmail.com>
2019-07-19 22:25:44 +0800

    :zap: 优化代码

--- src/test/java/me/zhyd/oauth/utils/CustomTest.java
+++ src/test/java/me/zhyd/oauth/utils/CustomTest.java
@@ -1,6 +1,15 @@
 package me.zhyd.oauth.utils;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.JSONPath;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author yadong.zhang (yadong.zhang0415(a)gmail.com)
@@ -59,5 +68,23 @@ public class CustomTest {
 //            System.out.println(stackTraceElement.getMethodName());
 //        }
         return stackTrace[2].getMethodName();
+    }
+
+    @Test
+    public void jsonpath(){
+        List<Map<String, Map<String, Object>>> list = new ArrayList<>();
+
+        Map<String, Map<String, Object>> map = new HashMap<>();
+        Map<String, Object> node = new HashMap<>();
+        node.put("emailAddress", "xxxx");
+        map.put("handle~", node);
+        list.add(map);
+
+
+        Map<String, Object> master = new HashMap<>();
+//        master.put("elements", list);
+        JSONObject emailObj = JSONObject.parseObject(JSON.toJSONString(master));
+        Object object = JSONPath.eval(emailObj, "$['elements'][0]['handle~']['emailAddress']");
+        System.out.println(object);
     }
 }
