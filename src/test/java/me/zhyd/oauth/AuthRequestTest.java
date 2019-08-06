cf77e4d0e0445a688cc2590df690f01392b15e3e
yadong.zhang <yadong.zhang0415@gmail.com>
2019-08-06 18:14:35 +0800

    :twisted_rightwards_arrows: 合并分支

--- src/test/java/me/zhyd/oauth/AuthRequestTest.java
+++ src/test/java/me/zhyd/oauth/AuthRequestTest.java
@@ -283,7 +283,22 @@ public class AuthRequestTest {
         AuthRequest authRequest = new AuthHuaweiRequest(AuthConfig.builder()
             .clientId("clientId")
             .clientSecret("clientSecret")
-            .redirectUri("redirectUri")
+            .redirectUri("http://redirectUri")
+            .build());
+        // 返回授权页面，可自行跳转
+        authRequest.authorize("state");
+        // 授权登录后会返回code（auth_code（仅限支付宝））、state，1.8.0版本后，可以用AuthCallback类作为回调接口的入参
+        // 注：JustAuth默认保存state的时效为3分钟，3分钟内未使用则会自动清除过期的state
+        AuthResponse login = authRequest.login(new AuthCallback());
+    }
+
+    @Test
+    public void weChatEnterpriseTest() {
+        AuthRequest authRequest = new AuthWeChatEnterpriseRequest(AuthConfig.builder()
+            .clientId("clientId")
+            .clientSecret("clientSecret")
+            .redirectUri("http://redirectUri")
+            .agentId("agentId")
             .build());
         // 返回授权页面，可自行跳转
         authRequest.authorize("state");
