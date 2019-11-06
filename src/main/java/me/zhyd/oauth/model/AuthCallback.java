cf48dfa6dfc29d2773f4386cb977183ed002870d
qizai <jnan77@qq.com>
2019-11-06 10:13:45 +0800

    fix#52 AuthCallback在spring中接收参数异常，需要有默认的构造方法

--- src/main/java/me/zhyd/oauth/model/AuthCallback.java
+++ src/main/java/me/zhyd/oauth/model/AuthCallback.java
@@ -1,8 +1,11 @@
 package me.zhyd.oauth.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import me.zhyd.oauth.enums.AuthUserGender;
 
 import java.io.Serializable;
 
@@ -15,6 +18,8 @@ import java.io.Serializable;
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthCallback implements Serializable {
 
     /**
