179287f7208b42af53a3f44661d60e8705c1ca6a
yadong.zhang <yadong.zhang0415@gmail.com>
2020-06-28 22:08:31 +0800

    :alien: 增加阿里云授权登录中刷新授权token的接口

--- src/main/java/me/zhyd/oauth/request/AuthAlipayRequest.java
+++ src/main/java/me/zhyd/oauth/request/AuthAlipayRequest.java
@@ -11,9 +11,11 @@ import com.alipay.api.response.AlipayUserInfoShareResponse;
 import me.zhyd.oauth.cache.AuthStateCache;
 import me.zhyd.oauth.config.AuthConfig;
 import me.zhyd.oauth.config.AuthDefaultSource;
+import me.zhyd.oauth.enums.AuthResponseStatus;
 import me.zhyd.oauth.enums.AuthUserGender;
 import me.zhyd.oauth.exception.AuthException;
 import me.zhyd.oauth.model.AuthCallback;
+import me.zhyd.oauth.model.AuthResponse;
 import me.zhyd.oauth.model.AuthToken;
 import me.zhyd.oauth.model.AuthUser;
 import me.zhyd.oauth.utils.StringUtils;
@@ -60,6 +62,37 @@ public class AuthAlipayRequest extends AuthDefaultRequest {
             .uid(response.getUserId())
             .expireIn(Integer.parseInt(response.getExpiresIn()))
             .refreshToken(response.getRefreshToken())
+            .build();
+    }
+
+    /**
+     * 刷新access token （续期）
+     *
+     * @param authToken 登录成功后返回的Token信息
+     * @return AuthResponse
+     */
+    @Override
+    public AuthResponse refresh(AuthToken authToken) {
+        AlipaySystemOauthTokenRequest request = new AlipaySystemOauthTokenRequest();
+        request.setGrantType("refresh_token");
+        request.setRefreshToken(authToken.getRefreshToken());
+        AlipaySystemOauthTokenResponse response = null;
+        try {
+            response = this.alipayClient.execute(request);
+        } catch (Exception e) {
+            throw new AuthException(e);
+        }
+        if (!response.isSuccess()) {
+            throw new AuthException(response.getSubMsg());
+        }
+        return AuthResponse.builder()
+            .code(AuthResponseStatus.SUCCESS.getCode())
+            .data(AuthToken.builder()
+                .accessToken(response.getAccessToken())
+                .uid(response.getUserId())
+                .expireIn(Integer.parseInt(response.getExpiresIn()))
+                .refreshToken(response.getRefreshToken())
+                .build())
             .build();
     }
 
