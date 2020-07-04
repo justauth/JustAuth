72bb1d826fc53879a5a870fbb30b3b826b5f0915
yadong.zhang <yadong.zhang0415@gmail.com>
2020-07-04 10:31:53 +0800

    :art: 完成 qq 的自定义 scope

---
+++ src/main/java/me/zhyd/oauth/enums/scope/AuthQqScope.java
@@ -0,0 +1,54 @@
+package me.zhyd.oauth.enums.scope;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * QQ 平台 OAuth 授权范围
+ *
+ * @author yadong.zhang (yadong.zhang0415(a)gmail.com)
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@Getter
+@AllArgsConstructor
+public enum AuthQqScope implements AuthScope {
+
+    /**
+     * {@code scope} 含义，以{@code description} 为准
+     */
+    GET_USER_INFO("get_user_info", "获取登录用户的昵称、头像、性别", true),
+    /**
+     * 以下 scope 需要申请：http://wiki.connect.qq.com/openapi%e6%9d%83%e9%99%90%e7%94%b3%e8%af%b7
+     */
+    GET_VIP_INFO("get_vip_info", "获取QQ会员的基本信息", false),
+    GET_VIP_RICH_INFO("get_vip_rich_info", "获取QQ会员的高级信息", false),
+    LIST_ALBUM("list_album", "获取用户QQ空间相册列表", false),
+    UPLOAD_PIC("upload_pic", "上传一张照片到QQ空间相册", false),
+    ADD_ALBUM("add_album", "在用户的空间相册里，创建一个新的个人相册", false),
+    LIST_PHOTO("list_photo", "获取用户QQ空间相册中的照片列表", false);
+
+    private String scope;
+    private String description;
+    private boolean isDefault;
+
+    public static List<AuthScope> getDefaultScopes() {
+        AuthQqScope[] scopes = AuthQqScope.values();
+        List<AuthScope> defaultScopes = new ArrayList<>();
+        for (AuthQqScope scope : scopes) {
+            if (scope.isDefault()) {
+                defaultScopes.add(scope);
+            }
+        }
+        return defaultScopes;
+    }
+
+    public static List<String> listScope() {
+        return Arrays.stream(AuthQqScope.values()).map(AuthQqScope::getScope).collect(Collectors.toList());
+    }
+}
