162a16820ddcaf32e77b2f24453c9d04dea297a1
yadong.zhang <yadong.zhang0415@gmail.com>
2020-07-04 13:17:23 +0800

    :alien: AuthScope + 包说明

---
+++ src/main/java/me/zhyd/oauth/enums/scope/AuthScope.java
@@ -0,0 +1,13 @@
+package me.zhyd.oauth.enums.scope;
+
+/**
+ * 各个平台 scope 类的统一接口
+ *
+ * @author yadong.zhang (yadong.zhang0415(a)gmail.com)
+ * @version 1.0.0
+ * @since 1.15.7
+ */
+public interface AuthScope {
+
+    String getScope();
+}
---
+++ src/main/java/me/zhyd/oauth/enums/scope/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 各个平台的授权范围
+ *
+ * @author yadong.zhang (yadong.zhang0415(a)gmail.com)
+ * @version 1.0.0
+ * @since 1.15.7
+ */
+package me.zhyd.oauth.enums.scope;
