10e445e87c2adf12d3505989d1f66020fa4af7a7
HeJin <2215990896@qq.com>
2021-01-09 20:38:33 +0800

    feat: 增加全局日志配置类

---
+++ src/main/java/me/zhyd/oauth/config/JustAuthLogConfig.java
@@ -0,0 +1,33 @@
+package me.zhyd.oauth.config;
+
+import me.zhyd.oauth.log.Log;
+
+/**
+ * JustAuth 日志配置类
+ * @author HeJin
+ * @date 2021/1/9 20:28
+ */
+public class JustAuthLogConfig {
+
+    /**
+     * 设置日志级别
+     * @param level 日志级别
+     */
+    public static void setLevel(Log.Level level){
+        Log.Config.level = level;
+    }
+
+    /**
+     * 关闭日志
+     */
+    public static void disable(){
+        Log.Config.enable = false;
+    }
+
+    /**
+     * 开启日志
+     */
+    public static void enable(){
+        Log.Config.enable = true;
+    }
+}
