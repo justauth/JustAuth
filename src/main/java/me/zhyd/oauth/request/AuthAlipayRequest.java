37b7784f89fd47c5f297a4d0c7d570a561f6b088
ngcly <531237716@qq.com>
2021-08-10 10:24:36 +0800

    格式化

--- src/main/java/me/zhyd/oauth/request/AuthAlipayRequest.java
+++ src/main/java/me/zhyd/oauth/request/AuthAlipayRequest.java
@@ -39,10 +39,10 @@ public class AuthAlipayRequest extends AuthDefaultRequest {
 
     public AuthAlipayRequest(AuthConfig config, AuthStateCache authStateCache) {
         super(config, AuthDefaultSource.ALIPAY, authStateCache);
-        if(StringUtils.isNotEmpty(config.getProxyHost()) && config.getProxyPort()!=null){
+        if (StringUtils.isNotEmpty(config.getProxyHost()) && config.getProxyPort() != null) {
             this.alipayClient = new DefaultAlipayClient(AuthDefaultSource.ALIPAY.accessToken(), config.getClientId(), config.getClientSecret(),
                 "json", "UTF-8", config.getAlipayPublicKey(), "RSA2", config.getProxyHost(), config.getProxyPort());
-        }else{
+        } else {
             this.alipayClient = new DefaultAlipayClient(AuthDefaultSource.ALIPAY.accessToken(), config.getClientId(), config.getClientSecret(), "json", "UTF-8", config
                 .getAlipayPublicKey(), "RSA2");
         }
@@ -59,7 +59,7 @@ public class AuthAlipayRequest extends AuthDefaultRequest {
         AlipaySystemOauthTokenRequest request = new AlipaySystemOauthTokenRequest();
         request.setGrantType("authorization_code");
         request.setCode(authCallback.getAuth_code());
-        AlipaySystemOauthTokenResponse response = null;
+        AlipaySystemOauthTokenResponse response;
         try {
             response = this.alipayClient.execute(request);
         } catch (Exception e) {
